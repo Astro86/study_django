e2877daacf6a250da2c2b3bb613331716d1e3dd6
양동우 <ckck803@naver.com>
2020-04-14 11:40:02 +0900

    django 지역(시간) 세팅 수정하기, Post 개선하기 str

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,8 @@ class Post(models.Model):
     create = models.DateTimeField()
     # 저자
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # 해당 객체를 문자열로 바꿧을 때 어떤 식으로 보여줄 것인지를 결정한다.
+    def __str__(self):
+        # 작성된 페이지의 제목과 저자를 보여준다.
+        return '{} :: {}'.format(self.title, self.author)
